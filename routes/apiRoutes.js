4da1097bd8865413117698eee062450ee11885a1
Dannar <dmawardi@gmail.com>
2019-09-24 20:32:12 +1000

    Fixed Prettier issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,7 +17,9 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
