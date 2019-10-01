e6dd9938f51c6cc7e2fc9339a8f734b18fd77588
josephcheok <joseph@cheokandco.com>
2019-10-01 21:03:23 +1000

    API routes, employee.js and timesheet.js updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,10 @@
 var db = require("../models");
-var bcrypt = require('bcrypt');
+var bcrypt = require("bcrypt");
 var passport = require("../config/passport");
 const saltRounds = 10;
-var fs = require('fs');
+var fs = require("fs");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // app.post("/api/login", function(req, res) {
   //   res.json(req.user);
   // });
@@ -17,75 +17,119 @@ module.exports = function (app) {
 
   // Register a new employer
   app.post("/api/register", (req, res) => {
-    
-    db.Employer.findOne({where: {employerEmail: req.body.employerEmail}})
-    .then(function (employer) {
+    db.Employer.findOne({
+      where: { employerEmail: req.body.employerEmail }
+    }).then(function(employer) {
       if (employer) {
-        res.send(false)
+        res.send(false);
       } else {
-        bcrypt.hash(req.body.employerPassword, saltRounds, function (err, hash) {
+        bcrypt.hash(req.body.employerPassword, saltRounds, function(err, hash) {
           db.Employer.create({
             employerEmail: req.body.employerEmail,
             employerName: req.body.employerName,
             employerPassword: hash,
             employerCompanyName: req.body.employerCompanyName
-          }).then(function (dbEmployer) {
-            
-            console.log("))))))))))))))))))))))))))))))))))))))))))))")
-            // res.redirect('/login')  
-            res.json(dbEmployer)  
-          }).catch((function (err) {
-            res.status(401).json(err);
-          }));
+          })
+            .then(function(dbEmployer) {
+              console.log("))))))))))))))))))))))))))))))))))))))))))))");
+              // res.redirect('/login')
+              res.json(dbEmployer);
+            })
+            .catch(function(err) {
+              res.status(401).json(err);
+            });
         });
       }
-
     });
   });
 
   // emploer login
-  app.post('/api/login',  (req, res) => {
+  app.post("/api/login", (req, res) => {
     // console.log(req.body.employerEmail)
     db.Employer.findOne({
       where: {
         employerEmail: req.body.employerEmail
       }
-    }).then(function (employer) {
+    }).then(function(employer) {
       if (!employer) {
-        res.send(false)
+        res.send(false);
         // res.redirect('/');
-       } else {
-        bcrypt.compare(req.body.employerPassword, employer.employerPassword, function (err, result) {
-          if (err) {
-            throw err
-          } else {
-            res.send(result);
+      } else {
+        bcrypt.compare(
+          req.body.employerPassword,
+          employer.employerPassword,
+          function(err, result) {
+            if (err) {
+              throw err;
+            } else {
+              res.send(result);
+            }
           }
-        });
-      }//
+        );
+      } //
     });
   });
 
-   // Route for getting some data about our user to be used client side
-   app.get("/api/employer_data", function(req, res) {
+  // Route for getting some data about our user to be used client side
+  app.get("/api/employer_data", function(req, res) {
+    console.log(
+      "lkajsdlkjf;asd ;NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN",
+      req
+    );
 
-     console.log("lkajsdlkjf;asd ;NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN", req)
-     
     // if (!req.user) {
     //   // The user is not logged in, send back an empty object
     //   res.json({});
     // } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      // res.json({
-      //   email: req.employer.employerEmail,
-      //   id: req.employer.id
-      // });
+    // Otherwise send back the user's email and id
+    // Sending back a password, even a hashed password, isn't a good idea
+    // res.json({
+    //   email: req.employer.employerEmail,
+    //   id: req.employer.id
+    // });
     // }
   });
 
+  app.get("/api/employees", function(req, res) {
+    db.Employee.findAll({}).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  app.post("/api/employees", function(req, res) {
+    db.Employee.create({
+      employeeName: req.body.employeeName,
+      employeeDepartment: req.body.employeeDepartment,
+      employeePosition: req.body.employeePosition,
+      employeeAddress: req.body.employeeAddress,
+      employeeContactNumber: req.body.employeeContactNumber,
+      employeeDOB: req.body.employeeDOB,
+      employeeImage: req.body.employeeImage
+    });
+
+    res.status(204).end();
+  });
+
+  app.get("/api/timesheet", function(req, res) {
+    db.Timesheet.findAll({}).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  app.post("/api/timesheet", function(req, res) {
+    db.Timesheet.create({
+      employeeID: req.body.employeeID,
+      employeeStatus: req.body.employeeStatus,
+      geoLocation: req.body.geoLocation,
+      check_in: req.body.check_in,
+      check_out: req.body.check_out
+    });
+
+    res.status(204).end();
+  });
+
   // Route for logging user out
-  app.get("/logout", function (req, res) {
+  app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
   });
@@ -110,7 +154,10 @@ module.exports = function (app) {
     // Read data file
 
     // Grab user's data
-    fs.readFile(path.join(__dirname, "../faceDB/facedb.txt"), 'utf8', function (err, data) {
+    fs.readFile(path.join(__dirname, "../faceDB/facedb.txt"), "utf8", function(
+      err,
+      data
+    ) {
       if (err) {
         throw err;
       }
@@ -119,7 +166,6 @@ module.exports = function (app) {
       console.log(faceToMatch);
       res.send(faceToMatch);
     });
-
   });
 
   // Create a new face
@@ -127,12 +173,13 @@ module.exports = function (app) {
     // Assign request body to facialModel
     var facialModel = req.body;
     // Insert facial model into database
-    fs.writeFile("faceDB/facedb.txt", JSON.stringify(facialModel), function(error) {
+    fs.writeFile("faceDB/facedb.txt", JSON.stringify(facialModel), function(
+      error
+    ) {
       if (error) throw res.sendStatus(500);
       console.log("File save");
     });
     // Send completed connection status
     res.sendStatus(200);
   });
-
 };
