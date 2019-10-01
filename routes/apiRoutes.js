fc6c942bb9b7ec129de04c0ce9d516dc50a31b6b
Dannar <dmawardi@gmail.com>
2019-10-01 13:55:09 +1000

    Committing first changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -82,4 +82,38 @@ module.exports = function (app) {
   //   });
   // });
 
+  // API Routes for face recognition
+  // API route for getting user's face model
+  app.get("/api/getCompanyFaceData", function(req, res) {
+    var fs = require("fs");
+    var path = require("path");
+    console.log("reading file");
+    // Read data file
+
+    // Grab user's data
+    fs.readFile(path.join(__dirname, "../faceDB/facedb.txt"), 'utf8', function (err, data) {
+      if (err) {
+        throw err;
+      }
+      // Print and return to user
+      var faceToMatch = data;
+      console.log(faceToMatch);
+      res.send(faceToMatch);
+    });
+
+  });
+
+  // Create a new face
+  app.post("/api/addNewFace", function(req, res) {
+    // Assign request body to facialModel
+    var facialModel = req.body;
+    // Insert facial model into database
+    fs.writeFile("faceDB/facedb.txt", JSON.stringify(facialModel), function(error) {
+      if (error) throw res.sendStatus(500);
+      console.log("File save");
+    });
+    // Send completed connection status
+    res.sendStatus(200);
+  });
+
 };
