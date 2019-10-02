080a0ba0b47df39fa890f6b3542a6a388dae2e71
eidmuha <eidmohd89@gmail.com>
2019-10-03 07:46:26 +1000

    login session

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -70,7 +70,7 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/dashboard", isAuthenticated, function(req, res) {
-    res.render("dashboard");
+    res.render("dashboard", {isAuthenticated: true});
   });
 
   // Face Recognition Pages
